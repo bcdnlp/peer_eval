7bfd85bf454fc1427f7edffa444895402bc6c34e
Xinya Du <xxd220001@csr-93596.utdallas.edu>
2023-06-18 11:17:22 -0500

    add gpt35_reviews_all and reviews_score version

---
+++ eval/gen_gpt3.5_reviews_score.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+# how to run this script 
+
+# ./gen_gpt3.5_reviews_score.sh generations/vicuna/answer_[Model 1].jsonl generations/vicuna/answer_[Model 2].jsonl
+
+echo "======================================== bard vs claude ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_bard.jsonl generations/vicuna/answer_claude.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_claude.jsonl generations/vicuna/answer_bard.jsonl
+
+echo "======================================== bard vs gpt4 ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_bard.jsonl generations/vicuna/answer_gpt4.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt4.jsonl generations/vicuna/answer_bard.jsonl
+
+echo "======================================== bard vs gpt35 ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_bard.jsonl generations/vicuna/answer_gpt35.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt35.jsonl generations/vicuna/answer_bard.jsonl
+
+echo "======================================== bard vs vicuna-13b ========================================"
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_bard.jsonl generations/vicuna/answer_vicuna-13b.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_vicuna-13b.jsonl generations/vicuna/answer_bard.jsonl
+
+echo "======================================== claude vs gpt4 ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_claude.jsonl generations/vicuna/answer_gpt4.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt4.jsonl generations/vicuna/answer_claude.jsonl
+
+echo "======================================== claude vs gpt35 ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_claude.jsonl generations/vicuna/answer_gpt35.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt35.jsonl generations/vicuna/answer_claude.jsonl
+
+echo "======================================== claude vs vicuna-13b ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_claude.jsonl generations/vicuna/answer_vicuna-13b.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_vicuna-13b.jsonl generations/vicuna/answer_claude.jsonl
+
+echo "======================================== gpt4 vs gpt35 ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt4.jsonl generations/vicuna/answer_gpt35.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt35.jsonl generations/vicuna/answer_gpt4.jsonl
+
+echo "======================================== gpt4 vs vicuna-13b ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt4.jsonl generations/vicuna/answer_vicuna-13b.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_vicuna-13b.jsonl generations/vicuna/answer_gpt4.jsonl
+
+echo "======================================== gpt35 vs vicuna-13b ========================================"
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_gpt35.jsonl generations/vicuna/answer_vicuna-13b.jsonl
+
+python eval_gpt_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_score.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-gpt3.5/score \
+    -m 'gpt-3.5-turbo-0301' \
+    -a generations/vicuna/answer_vicuna-13b.jsonl generations/vicuna/answer_gpt35.jsonl
