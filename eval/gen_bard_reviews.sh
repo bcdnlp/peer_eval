987737560c25ddd64d7f4fc8599039f8acf98141
Xinya Du <xxd220001@csr-93596.utdallas.edu>
2023-06-18 17:53:34 -0500

    add ratings-bard and eval scripts

---
+++ eval/gen_bard_reviews.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# how to run this script 
+
+# ./gen_gpt4_reviews.sh generations/vicuna/answer_claude.jsonl generations/vicuna/answer_gpt4.jsonl
+# claude vs {gpt4, gpt35, bard, vicuna-13b} 
+
+python eval_bard_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_threeclass.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-bard \
+    -a $1 $2
+
+python eval_bard_review.py \
+    -q prompts/vicuna_questions.jsonl \
+    -p prompts/vicuna_prompt_threeclass.jsonl \
+    -r prompts/reviewer.jsonl \
+    -o ratings-bard \
+    -a $2 $1
