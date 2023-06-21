b7f49a4412cf95ea5b5963ee674468d350a30511
Teerth Laptop <teerthp2004@gmail.com>
2023-06-20 20:31:51 -0500

    claude is first model for mturk input

--- make_mturk_input.sh
+++ make_mturk_input.sh
@@ -5,8 +5,8 @@ GEN_PATH=./eval/generations/vicuna
 python ./generate_mturk_input.py \
     -q ./eval/prompts/vicuna_questions.jsonl \
     -a \
-    $GEN_PATH/answer_bard.jsonl \
     $GEN_PATH/answer_claude.jsonl \
+    $GEN_PATH/answer_bard.jsonl \
     $GEN_PATH/answer_gpt4.jsonl \
     $GEN_PATH/answer_gpt35.jsonl \
     $GEN_PATH/answer_vicuna-13b.jsonl \
